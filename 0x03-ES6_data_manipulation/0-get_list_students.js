1f46eb64e0b1c197ef8766023fef07dd37bd21ee
SaintAlaye <elitetechydev@gmail.com>
2024-01-09 07:49:05 +0000

    return an array of students and their info

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const object1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const object2 = { id: 2, firstName: 'James', location: 'Colombia' };
+  const object3 = { id: 3, firstName: 'Serena', location: 'San Francisco' };
+
+  return [object1, object2, object3];
+}
